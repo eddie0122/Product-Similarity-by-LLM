54bfab7478e8db19d3e6baff6af339714cccbe5b
hanul <pearljamlove@gmail.com>
2025-09-30 07:17:48 +0000

    Feat: Create product integrated information table

--- 00_create_table.sql
+++ 00_create_table.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA product_similarity;
+CREATE SCHEMA IF NOT EXISTS product_similarity;
 
 CREATE TABLE IF NOT EXISTS product_similarity.product_raw (
     prd_id VARCHAR(30) PRIMARY KEY,
@@ -18,4 +18,20 @@ CREATE TABLE IF NOT EXISTS product_similarity.products_trait_image (
     material VARCHAR(50) NOT NULL,
     occasion VARCHAR(50) NOT NULL,
     prd_id VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS product_similarity.products_trait_information (
+    prd_id VARCHAR(30) NOT NULL,
+    category VARCHAR(20) NOT NULL,
+    prd_name TEXT NOT NULL,
+    price NUMERIC NOT NULL,
+    review NUMERIC,
+    review_rating NUMERIC,
+    prd_img_path TEXT,
+    category_high_img VARCHAR(30) NOT NULL,
+    category_low_img VARCHAR(30) NOT NULL,
+    color_img VARCHAR(50) NOT NULL,
+    style_img VARCHAR(50) NOT NULL,
+    material_img VARCHAR(50) NOT NULL,
+    occasion_img VARCHAR(50) NOT NULL
 );
