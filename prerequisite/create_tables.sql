f620dbbc92f0fe1a86e156a80c27e0a5be343571
hanul <pearljamlove@gmail.com>
2025-12-03 18:04:55 +0900

    Feat: Add new table for dropping duplicate product names

--- prerequisite/create_tables.sql
+++ prerequisite/create_tables.sql
@@ -10,6 +10,16 @@ CREATE TABLE IF NOT EXISTS product_similarity.product_raw (
     prd_img TEXT
 );
 
+CREATE TABLE IF NOT EXISTS product_similarity.product_information (
+    prd_id VARCHAR(30) PRIMARY KEY,
+    category VARCHAR(20),
+    prd_name TEXT,
+    price NUMERIC,
+    review NUMERIC,
+    review_rating NUMERIC,
+    prd_img TEXT
+);
+
 CREATE TABLE IF NOT EXISTS product_similarity.products_trait_image (
     category1 VARCHAR(30),
     category2 VARCHAR(30),
